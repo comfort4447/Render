f2c36dc3587a6f1dc6b04684235659f57c8b0403
ABISOLA TAYO <tayoabisola44@gmail.com>
2023-02-17 12:34:31 +0100

    DATABASE_URL

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgres://postgres_deployment_example_amry_user:uEI3Qg4V2QaCajmMTOB4uwq7MwnLCoXb@dpg-cflqlp1gp3ju5h5pfeb0-a/postgres_deployment_example_amry"
+export DATABASE_URL="postgres://postgres_deployment_example_eto3_user:Qetr0rIHqb4klKjBgEGw8LCN6yTyqSJp@dpg-cfnmb82rrk0eqlpvmghg-a/postgres_deployment_example_eto3"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
